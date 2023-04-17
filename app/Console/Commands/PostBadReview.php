c6c44b5c4c322ca9b88b578003f3a9aa49d8048b
huxiaochun <396984774@qq.com>
2023-04-17 17:14:55 +0800

    修改一些参数

--- app/Console/Commands/PostBadReview.php
+++ app/Console/Commands/PostBadReview.php
@@ -102,10 +102,10 @@ class PostBadReview extends Command
                     $driver->get($post_url);
                     sleep(3);
                     //举报
-                    $driver->findElement(WebDriverBy::cssSelector('.x1ey2m1c.xds687c.xg01cxk.x47corl.x10l6tqk.x17qophe.x13vifvy.x1ebt8du.x19991ni.x1dhq9h.x1wpzbip.x14yjl9h.xudhj91.x18nykt9.xww2gxu')) // find search input element
+                    $driver->findElements(WebDriverBy::cssSelector('.x1i10hfl.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x1ypdohk.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x16tdsg8.x1hl2dhg.xggy1nq.x87ps6o.x1lku1pv.x1a2a7pz.x6s0dn4.x14yjl9h.xudhj91.x18nykt9.xww2gxu.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x78zum5.xl56j7k.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.xc9qbxq.x14qfxbe.x1qhmfi1'))[1] // find search input element
                     ->click();
                     sleep(3);
-                    $driver->findElement(WebDriverBy::xpath("//*[contains(text(), 'Report post')]"))->click();
+                    $driver->findElement(WebDriverBy::xpath("//*[contains(text(), 'Report video')]"))->click();
                     sleep(3);
                     $driver->findElement(WebDriverBy::xpath("//*[contains(text(), 'False information')]"))->click();
                     sleep(3);
